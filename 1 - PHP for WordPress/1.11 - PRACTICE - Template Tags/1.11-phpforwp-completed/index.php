8f2e90c63d3c7ae12ef2a262cdc27dc4111aebbd
Zac Gordon <education@zacgordon.com>
2017-04-23 14:22:16 -0400

    Tabs and spaces

--- 1 - PHP for WordPress/1.11 - PRACTICE - Template Tags/1.11-phpforwp-completed/index.php
+++ 1 - PHP for WordPress/1.11 - PRACTICE - Template Tags/1.11-phpforwp-completed/index.php
@@ -1,50 +1,50 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>PHP for WordPress</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
-		<link rel="stylesheet" href="/wp-content/themes/1.11-phpforwp-completed/style.css">
-	</head>
-  <body class="<?php body_class(); ?>">
+<head>
+  <meta charset="utf-8">
+  <title>PHP for WordPress</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
+  <link rel="stylesheet" href="/wp-content/themes/1.11-phpforwp-completed/style.css">
+</head>
+<body class="<?php body_class(); ?>">
 
-    <header id="masthead">
-    	<h1><a href="#">PHP for WordPress</a></h1>
-    </header>
+  <header id="masthead">
+    <h1><a href="#">PHP for WordPress</a></h1>
+  </header>
 
-    <div id="content">
+  <div id="content">
 
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-				<article class="<?php post_class(); ?>">
+      <article class="<?php post_class(); ?>">
 
-					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-					<p class="byline">
-					</p>
+        <?php the_content(); ?>
 
-					<?php the_content(); ?>
+        <footer>
+          <p class="byline">
+            Author:
+            <a href="<?php the_author(); ?>"><?php the_author(); ?></a> |
+            Date:
+            <?php the_time( 'M. j, Y' ); ?> |
+            Categories:
+            <?php the_category( ',' ); ?> |
+            Tags:
+            <?php the_tags( '', ',', '' ); ?>
+          </p>
+        </footer>
 
-					<footer>
-						<p class="byline">
-							Author:
-							<a href="<?php the_author(); ?>"><?php the_author(); ?></a> |
-							Date: <?php the_time( 'M. j, Y' ); ?> |
-							Categories: <?php the_category( ',' ); ?> |
-							Tags: <?php the_tags( '', ',', '' ); ?>
-						</p>
-					</footer>
+      </article>
 
-				</article>
+    <?php endwhile; else: ?>
 
-			<?php endwhile; else: ?>
+      <h2><?php _e( '404 Error' ); ?></h2>
+      <p><?php _e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
 
-				<h2><?php _e( '404 Error' ); ?></h2>
-				<p><?php _e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
+    <?php endif; ?>
 
-			<?php endif; ?>
+  </div>
 
-		</div>
-
-	</body>
+</body>
 </html>
