091acf17880c893a13fc2afa3ad78a3962bd17fc
Zac Gordon <education@zacgordon.com>
2017-04-23 14:26:54 -0400

    Tabs and spaces

--- 1 - PHP for WordPress/1.13 - PRACTICE - Conditional Tags/1.13-phpforwp-starter/index.php
+++ 1 - PHP for WordPress/1.13 - PRACTICE - Conditional Tags/1.13-phpforwp-starter/index.php
@@ -4,36 +4,36 @@
     <meta charset="utf-8">
     <title>PHP for WordPress</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
-		<link rel="stylesheet" href="/wp-content/themes/1.13-phpforwp-starter/style.css">
-	</head>
+    <link rel="stylesheet" href="/wp-content/themes/1.13-phpforwp-starter/style.css">
+  </head>
   <body class="<?php body_class(); ?>">
 
     <header id="masthead">
-    	<h1><a href="#">PHP for WordPress</a></h1>
+      <h1><a href="#">PHP for WordPress</a></h1>
     </header>
 
     <div id="content">
 
-			<!-- Static Front Page -->
+      <!-- Static Front Page -->
 
-			<!-- Blog Home -->
+      <!-- Blog Home -->
 
-			<!-- Page (Not Front Page) -->
+      <!-- Page (Not Front Page) -->
 
-			<!-- Single Post -->
+      <!-- Single Post -->
 
-			<!-- Single Attachment (Media) -->
+      <!-- Single Attachment (Media) -->
 
-			<!-- Category Archive -->
+      <!-- Category Archive -->
 
-			<!-- Tag Archive -->
+      <!-- Tag Archive -->
 
-			<!-- Author Archive -->
+      <!-- Author Archive -->
 
-			<!-- Date Archive -->
+      <!-- Date Archive -->
 
-			<!-- 404 Page -->
+      <!-- 404 Page -->
 
-		</div>
-	</body>
+    </div>
+  </body>
 </html>
