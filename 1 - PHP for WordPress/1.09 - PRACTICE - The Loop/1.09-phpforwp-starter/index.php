0da059c6b17e666ddfe4b46cfb5f6c5f5d44d769
Zac Gordon <education@zacgordon.com>
2017-04-23 14:17:53 -0400

    Tabs and spaces

--- 1 - PHP for WordPress/1.09 - PRACTICE - The Loop/1.09-phpforwp-starter/index.php
+++ 1 - PHP for WordPress/1.09 - PRACTICE - The Loop/1.09-phpforwp-starter/index.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>PHP for WordPress</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
-		<link rel="stylesheet" href="/wp-content/themes/1.9-phpforwp-starter/style.css">
-	</head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <title>PHP for WordPress</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
+  <link rel="stylesheet" href="/wp-content/themes/1.9-phpforwp-starter/style.css">
+</head>
+<body>
 
-    <header id="masthead">
-    	<h1><a href="#">PHP for WordPress</a></h1>
-    </header>
+  <header id="masthead">
+  	<h1><a href="#">PHP for WordPress</a></h1>
+  </header>
 
-    <div id="content">
+  <div id="content">
 
-			<?php // Start the loop here ?>
+    <?php // Start the loop here ?>
 
-				<!-- Display the_title and the_content here -->
+      <!-- Display the_title and the_content here -->
 
-			<?php // End while and start else ?>
+    <?php // End while and start else ?>
 
-				<!-- Display a 404 message here -->
+      <!-- Display a 404 message here -->
 
-			<?php // End if ?>
+    <?php // End if ?>
 
-		</div>
+  </div>
 
-	</body>
+</body>
 </html>
