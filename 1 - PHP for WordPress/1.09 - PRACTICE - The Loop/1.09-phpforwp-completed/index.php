83565f1a9bf04d508f567e2058fd1ed75640ec46
Zac Gordon <education@zacgordon.com>
2017-04-23 14:19:43 -0400

    Tabs and spaces

--- 1 - PHP for WordPress/1.09 - PRACTICE - The Loop/1.09-phpforwp-completed/index.php
+++ 1 - PHP for WordPress/1.09 - PRACTICE - The Loop/1.09-phpforwp-completed/index.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>PHP for WordPress</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
+<head>
+  <meta charset="utf-8">
+  <title>PHP for WordPress</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
   <link rel="stylesheet" href="/wp-content/themes/1.9-phpforwp-completed/style.css">
-  </head>
-  <body>
+</head>
+<body>
 
-    <header id="masthead">
-      <h1><a href="#">PHP for WordPress</a></h1>
-    </header>
+  <header id="masthead">
+    <h1><a href="#">PHP for WordPress</a></h1>
+  </header>
 
-    <div id="content">
+  <div id="content">
 
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
+      <h2><?php the_title(); ?></h2>
+      <?php the_content(); ?>
 
-      <?php endwhile; else: ?>
+    <?php endwhile; else: ?>
 
-        <h2><?php _e( '404 Error' ); ?></h2>
-        <p><?php _e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
+      <h2><?php _e( '404 Error' ); ?></h2>
+      <p><?php _e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
 
-      <?php endif; ?>
+    <?php endif; ?>
 
-    </div>
+  </div>
 
-  </body>
+</body>
 </html>
