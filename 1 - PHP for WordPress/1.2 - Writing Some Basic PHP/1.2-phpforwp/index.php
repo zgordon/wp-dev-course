04235da9dfe5a8804e64f79ce8f0f5ca72d92a41
Zac Gordon <education@zacgordon.com>
2017-04-22 21:42:44 -0400

    Adding a function to the example theme

--- 1 - PHP for WordPress/1.2 - Writing Some Basic PHP/1.2-phpforwp/index.php
+++ 1 - PHP for WordPress/1.2 - Writing Some Basic PHP/1.2-phpforwp/index.php
@@ -1,9 +1,36 @@
-<?php
+<link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
+<link rel="stylesheet" href="/wp-content/themes/1.2-phpforwp/style.css">
 
-	$your_name = "Enter your name";
+<header id="masthead">
+	<h1><a href="#">PHP for WordPress</a></h1>
+</header>
 
- ?>
+<div id="content">
 
- <h1>Welcome</h1>
+	<?php
 
- <p>My name is <?php echo $your_name; ?></p>
+		$name = "Enter your name";
+
+	?>
+
+	<h2>Welcome!</h2>
+
+	<p>My name is "<?php echo $name; ?>."</p>
+
+
+	<h2>My Latest Post:</h2>
+
+	<?php
+
+		display_post( 'Hello PHP!', 'Lorem ipsum content' );
+
+		function display_post( $title, $content ) {
+
+			echo "<h3><a href=\"#\">$title</a></h3>";
+			echo "<div>$content</div>";
+
+		}
+
+	?>
+
+</div>
