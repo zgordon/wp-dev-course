88f5881dd4e263e6416c7a293c422e6174620901
Zac Gordon <education@zacgordon.com>
2017-04-23 14:27:30 -0400

    Tabs and spaces

--- 1 - PHP for WordPress/1.15 - PRACTICE - WordPress Hooks/1.15-phpforwp-completed/functions.php
+++ 1 - PHP for WordPress/1.15 - PRACTICE - WordPress Hooks/1.15-phpforwp-completed/functions.php
@@ -5,9 +5,9 @@
  */
 function phpforwp_theme_styles() {
 
-	wp_enqueue_script( 'font-css', 'https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round' );
+  wp_enqueue_script( 'font-css', 'https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round' );
 
-	wp_enqueue_style( 'main-css', get_stylesheet_uri(), 'fonts-css', get_the_time() );
+  wp_enqueue_style( 'main-css', get_stylesheet_uri(), 'fonts-css', get_the_time() );
 
 }
 add_action( 'wp_enqueue_scripts', 'phpforwp_theme_styles' );
@@ -21,9 +21,9 @@ add_action( 'wp_enqueue_scripts', 'phpforwp_theme_styles' );
  */
 function phpforwp_read_more_link( $excerpt ) {
 
-	$extended_excerpt = $excerpt;
+  $extended_excerpt = $excerpt;
 
-	$extended_excerpt .= ' <a class="more-link" href="' . get_permalink() . '">Read more &raquo;</a>';
+  $extended_excerpt .= ' <a class="more-link" href="' . get_permalink() . '">Read more &raquo;</a>';
 
   return $extended_excerpt;
 
