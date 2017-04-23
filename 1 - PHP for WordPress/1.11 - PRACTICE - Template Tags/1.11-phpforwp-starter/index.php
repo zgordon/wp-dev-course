51212e61143efb1dcdc59fb10d3d8f85ef0f3a66
Zac Gordon <education@zacgordon.com>
2017-04-23 14:23:37 -0400

    Tabs and spaces

--- 1 - PHP for WordPress/1.11 - PRACTICE - Template Tags/1.11-phpforwp-starter/index.php
+++ 1 - PHP for WordPress/1.11 - PRACTICE - Template Tags/1.11-phpforwp-starter/index.php
@@ -1,37 +1,35 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>PHP for WordPress</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
-		<link rel="stylesheet" href="/wp-content/themes/1.11-phpforwp-starter/style.css">
-	</head>
+<head>
+  <meta charset="utf-8">
+  <title>PHP for WordPress</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
+  <link rel="stylesheet" href="/wp-content/themes/1.11-phpforwp-starter/style.css">
+</head>
+<!-- Add any template tags outside of loop -->
+<body>
 
-	<!-- Add any template tags outside of loop -->
+  <header id="masthead">
+    <h1><a href="#">PHP for WordPress</a></h1>
+  </header>
 
-  <body>
+  <div id="content">
 
-    <header id="masthead">
-    	<h1><a href="#">PHP for WordPress</a></h1>
-    </header>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <div id="content">
+      <!-- Add any post template tags inside of loop -->
 
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <h2><?php the_title(); ?></h2>
+      <?php the_content(); ?>
 
-				<!-- Add any post template tags inside of loop -->
+    <?php endwhile; else: ?>
 
-				<h2><?php the_title(); ?></h2>
-				<?php the_content(); ?>
+      <h2><?php _e( '404 Error' ); ?></h2>
+      <p><?php _e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
 
-			<?php endwhile; else: ?>
+    <?php endif; ?>
 
-				<h2><?php _e( '404 Error' ); ?></h2>
-				<p><?php _e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
+  </div>
 
-			<?php endif; ?>
-
-		</div>
-
-	</body>
+</body>
 </html>
