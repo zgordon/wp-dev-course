99df1e59c339fc96aff7a93632681c19d3678265
Zac Gordon <education@zacgordon.com>
2017-04-23 14:27:48 -0400

    Tabs and spaces

--- 1 - PHP for WordPress/1.15 - PRACTICE - WordPress Hooks/1.15-phpforwp-completed/index.php
+++ 1 - PHP for WordPress/1.15 - PRACTICE - WordPress Hooks/1.15-phpforwp-completed/index.php
@@ -3,49 +3,49 @@
   <head>
     <meta charset="utf-8">
     <title>PHP for WordPress</title>
-		<?php wp_head(); ?>
-	</head>
+    <?php wp_head(); ?>
+  </head>
   <body class="<?php body_class(); ?>">
 
     <header id="masthead">
-    	<h1><a href="#">PHP for WordPress</a></h1>
+      <h1><a href="#">PHP for WordPress</a></h1>
     </header>
 
     <div id="content">
 
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-				<article class="<?php post_class(); ?>">
+        <article class="<?php post_class(); ?>">
 
-					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-					<p class="byline">
-					</p>
+          <p class="byline">
+          </p>
 
-					<?php the_excerpt(); ?>
+          <?php the_excerpt(); ?>
 
-					<footer>
-						<p class="byline">
-							Author:
-							<a href="<?php the_author(); ?>"><?php the_author(); ?></a> |
-							Date: <?php the_time( 'M. j, Y' ); ?> |
-							Categories: <?php the_category( ',' ); ?> |
-							Tags: <?php the_tags( '', ',', '' ); ?>
-						</p>
-					</footer>
+          <footer>
+            <p class="byline">
+              Author:
+              <a href="<?php the_author(); ?>"><?php the_author(); ?></a> |
+              Date: <?php the_time( 'M. j, Y' ); ?> |
+              Categories: <?php the_category( ',' ); ?> |
+              Tags: <?php the_tags( '', ',', '' ); ?>
+            </p>
+          </footer>
 
-				</article>
+        </article>
 
-			<?php endwhile; else: ?>
+      <?php endwhile; else: ?>
 
-				<h2><?php _e( '404 Error' ); ?></h2>
-				<p><?php _e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
+        <h2><?php _e( '404 Error' ); ?></h2>
+        <p><?php _e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
 
-			<?php endif; ?>
+      <?php endif; ?>
 
-		</div>
+    </div>
 
-		<?php wp_footer(); ?>
+    <?php wp_footer(); ?>
 
-	</body>
+  </body>
 </html>
