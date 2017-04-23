f53ac394871d1a159382b9079426eb40e6d94f65
Zac Gordon <education@zacgordon.com>
2017-04-23 14:00:47 -0400

    Tabs and spaces

--- 1 - PHP for WordPress/1.04 - PRACTICE - PHP Basics/1.04-phpforwp-starter/index.php
+++ 1 - PHP for WordPress/1.04 - PRACTICE - PHP Basics/1.04-phpforwp-starter/index.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>PHP for WordPress</title>
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
-		<link rel="stylesheet" href="/wp-content/themes/1.04-phpforwp-starter/style.css">
-	</head>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
+    <link rel="stylesheet" href="/wp-content/themes/1.04-phpforwp-starter/style.css">
+  </head>
   <body>
 
     <header id="masthead">
@@ -14,98 +14,97 @@
 
     <div id="content">
 
-			<h2>A Post Object</h2>
+      <h2>A Post Object</h2>
 
-			<?php
+      <?php
 
-				// Create a new object and save as $post
-				// Assign a title and content property to $post
-
-
-				// Use pre_export to display the $post
-
-			?>
+      // Create a new object and save as $post
+      // Assign a title and content property to $post
 
 
-			<h2>Array of Post Objects</h2>
+      // Use pre_export to display the $post
 
-			<?php
-
-				/**
-				 * Custom function for creating post objects
-				 *
-				 * @param string $title Title for the post
-				 * @param string $content Content for the post with HTML
-				 * @return object Post object
-				 */
-				function create_post( $title, $content ) {
-
-					// Create a new object and save as $post
-					// Assign a $title and $content as property to $post
+      ?>
 
 
-					// Return the $post
+      <h2>Array of Post Objects</h2>
 
-				}
+      <?php
 
-				// Create an array of post objects using the create_post function
-				$posts = array(
-					// Post 1
-					// Post 2
-					// Post 3
-				);
+      /**
+       * Custom function for creating post objects
+       *
+       * @param string $title Title for the post
+       * @param string $content Content for the post with HTML
+       * @return object Post object
+       */
+      function create_post( $title, $content ) {
 
-				// Use pre_export to display the $posts
-
-			?>
-
-			<h2>Displaying Post Objects</h2>
-
-
-			<?php
-
-				// Loop through array of posts and display each one on the page
-				foreach( $posts as $post ) {
-
-					// Call the display_post function and pass it the $post
-
-				}
-
-				/**
-				 * Custom function for displaying the title and content for a post
-				 *
-				 * @param object $post The post to be displayed
-				 */
-				function display_post( $post ) {
-
-					// Echo an <h3> tag with the $post->title inside
-					// Echo a <div> tag with the $post->content inside
-
-				}
-
-			?>
+      // Create a new object and save as $post
+      // Assign a $title and $content as property to $post
 
 
-			<?php
+      // Return the $post
+
+      }
+
+      // Create an array of post objects using the create_post function
+      $posts = array(
+      // Post 1
+      // Post 2
+      // Post 3
+      );
+
+      // Use pre_export to display the $posts
+
+      ?>
+
+      <h2>Displaying Post Objects</h2>
 
 
-			/**
-			 * Helper function for var_exporting objects wrapped in <pre> tags
-			 *
-			 * @param object|array $content The content to be var_exported
-			 */
-			function pre_export( $content ) {
+      <?php
 
-					echo "<pre>";
-					var_export( $content );
-					echo "</pre>";
+      // Loop through array of posts and display each one on the page
+      foreach( $posts as $post ) {
 
-			}
+      // Call the display_post function and pass it the $post
 
-			?>
+      }
+
+      /**
+       * Custom function for displaying the title and content for a post
+       *
+       * @param object $post The post to be displayed
+       */
+      function display_post( $post ) {
+
+      // Echo an <h3> tag with the $post->title inside
+      // Echo a <div> tag with the $post->content inside
+
+      }
+
+      ?>
 
 
-		</div>
+      <?php
 
-	</body>
+
+      /**
+       * Helper function for var_exporting objects wrapped in <pre> tags
+       *
+       * @param object|array $content The content to be var_exported
+       */
+      function pre_export( $content ) {
+
+      echo "<pre>";
+      var_export( $content );
+      echo "</pre>";
+
+      }
+
+      ?>
+
+  </div>
+
+</body>
 </html>
