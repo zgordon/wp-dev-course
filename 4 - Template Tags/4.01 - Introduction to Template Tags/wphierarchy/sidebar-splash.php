42e41bd0ab90dd91f26a8e353b7a1860c2e178ed
Zac Gordon <education@zacgordon.com>
2017-08-21 12:37:11 -0400

    Removing last of old themes

--- 4 - Template Tags/4.01 - Introduction to Template Tags/wphierarchy/sidebar-splash.php
+++
@@ -1,5 +0,0 @@
-<aside id="secondary" class="widget-area" role="complementary">
-
-  <p>Sell something major!</p>
-
-</aside>
--- 4 - Template Tags/wphierarchy-starter/assets/js/theme.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  var projectButton = document.querySelector( '.single-portfolio a.button' );
-  projectButton.target = '_blank';
-})();
